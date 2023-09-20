13fbf5c1039ce8b447cb7b5de78e586aa4719489
Keyser Durden <41450293+Keyser-Durden@users.noreply.github.com>
2023-09-19 21:57:31 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,3 +12,23 @@ wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo tee 
 sudo apt update
 sudo apt install -y postgresql postgresql-contrib
 
+# Check that the postgress is opening the port on localhost
+sudo ss -lnpt | grep postgres
+
+# Start postgres if it is not running
+sudo systemctl start postgresql
+
+# Become postgresql user and setup database
+sudo -u postgres -i psql
+# This should be done via cmdline from inside psql
+# CREATE DATABASE mastodon;
+# CREATE USER mastodon;
+# ALTER USER mastodon WITH ENCRYPTED PASSWORD 'your_preferred_password';
+# ALTER USER mastodon createdb;
+# ALTER DATABASE mastodon OWNER TO mastodon;
+# \q
+
+
+
+
+
