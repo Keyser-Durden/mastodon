2940acea5b7224f1f542801712cbec3c45025a33
Keyser Durden <41450293+Keyser-Durden@users.noreply.github.com>
2023-09-19 21:53:39 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 source settings.conf
+
+# Add the upstream repository.
+echo "deb [signed-by=/etc/apt/keyrings/postgresql.asc] http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list
+
+# Import the PostgreSQL public key.
+sudo mkdir -p /etc/apt/keyrings/
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo tee /etc/apt/keyrings/postgresql.asc
+
+# Update repository index and install PostgreSQL.
+sudo apt update
+sudo apt install -y postgresql postgresql-contrib
+
