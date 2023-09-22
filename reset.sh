376004c2fa765ff9699409c11e8e0fb81318d9c6
Keyser Durden <41450293+Keyser-Durden@users.noreply.github.com>
2023-09-22 19:01:54 -0400

    Update reset.sh

--- reset.sh
+++ reset.sh
@@ -2,7 +2,6 @@
 source settings.conf
 
 cd $code_path
-cp $code_path/mastodon/settings.conf $code_path/
 rm -f $code_path/mastodon
 git clone $git_url
-cp  $code_path/settings.conf $code_path/mastodon/settings.conf
+#cp  $code_path/settings.conf $code_path/mastodon/settings.conf
