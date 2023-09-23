b3cde8ddb6ffd3fc80b2359f10ec622b64d29a68
SquanchIt <141355723+SquanchIt@users.noreply.github.com>
2023-09-23 18:16:44 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -28,20 +28,20 @@ sudo apt install -y postgresql postgresql-contrib
 
 setup_db() {
   # modify psql conf file to allow root to access w/o password
-  #local pg_hba_file="/etc/postgresql/14/main/pg_hba.conf"
-  #local new_hba_file="$pg_hba_file.bak"
+  local pg_hba_file="/etc/postgresql/14/main/pg_hba.conf"
+  local pg_hba_backup="$pg_hba_file.bak"
  
 # Backup the original pg_hba.conf if not already backed up
-  #if [ ! -f "$backup_file" ]; then
-   # cp "$pg_hba_file" "$new_hba_file"
-  #fi
+  if [ ! -f "$backup_file" ]; then
+    cp "$pg_hba_file" "$pg_hba_backup"
+  fi
   
-#cat $new_hba_file | sed '/postgres/s/peer/trust/g' > $pg_hba_file
+cat $pg_hba_backup | sed '/postgres/s/peer/trust/g' > $pg_hba_file
 
 # Add a new line to allow root to access without a password (replace with the desired line)
 #echo "local all postgres trust" >> "$pg_hba_file"
 
-#systemctl reload postgresql
+systemctl reload postgresql
   
   sudo -u postgres bash <<EOF
   while IFS= read -r line; do
@@ -54,7 +54,7 @@ setup_db() {
 EOF
 
   #systemctl reload postgresql
-  #cp -f $new_hba_file $pg_hba_file
+  cp -f $pg_hba_backup $pg_hba_file
 }
 
 setup_db
